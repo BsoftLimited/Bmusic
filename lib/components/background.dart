2ba5c05d4674490fa618f40b8ad717caa7c6281d
lordBman <okelekelenobel@gmail.com>
2024-07-23 11:00:59 +0100

    successfully added the blur background

--- lib/components/background.dart
+++ lib/components/background.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 
-import 'package:bmusic/notifier/playing.dart';
 import 'package:bmusic/notifier/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
